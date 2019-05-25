7ec66f17cb3082b3c3dd4c387fc0b026853e34f7
reisvictor1 <36137949+reisvictor1@users.noreply.github.com>
2019-05-25 11:30:28 -0300

    A grande cola aparece das trevas, o BD atualizado pro user

--- Banco_Dados/poo_bd.sql
+++ Banco_Dados/poo_bd.sql
@@ -6,15 +6,15 @@ CREATE TABLE if not exists pets(
 	
     pet_id INT NOT NULL,
 	species varchar(20) NOT NULL
-
+	
 )
 
 CREATE TABLE if not exists clientes(
 	
     cliente_id INT NOT NULL,
-    nomelogin VARCHAR(30) NOT NULL,
-    senha VARCHAR(75) NOT NULL,	
-    nome VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    senha VARCHAR(75) NOT NULL,
+	nome VARCHAR(50) NOT NULL,
     idade int NOT NULL,
     cpf varchar(15) NOT NULL,
     cidade VARCHAR(50) NOT NULL,
@@ -28,3 +28,6 @@ CREATE TABLE IF NOT EXISTS adocao(
     cliente_id int NOT NULL
 
 )
+
+
+
