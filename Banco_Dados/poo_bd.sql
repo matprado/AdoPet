048c0bfcadacdcc999cb6654255b4d4b3b5c851e
reisvictor1 <36137949+reisvictor1@users.noreply.github.com>
2019-06-13 13:11:59 -0300

    BD totalmente operacional

--- Banco_Dados/poo_bd.sql
+++ Banco_Dados/poo_bd.sql
@@ -1,36 +1,38 @@
-CREATE DATABASE project
+CREATE DATABASE project;
 
-USE project
+USE project;
 
 CREATE TABLE if not exists pets(
 	
-    pet_id LONG NOT NULL auto AUTO_INCREMENT,
+    pet_id INTEGER NOT NULL AUTO_INCREMENT primary key,
 	species varchar(20) NOT NULL,
     nome varchar(50) NOT NULL,
     sexo VARCHAR(20) NOT NULL,
     detalhes VARCHAR(100) not null,
     id_doador LONG NOT NULL,
     caminho_imagem_pet blob
-)
+);
 
 ALTER TABLE pets ADD UNIQUE(pet_id);
-ALTER TABLE pets ADD PRIMARY KEY (pet_id)
+ALTER TABLE pets ADD PRIMARY KEY (pet_id);
 
 CREATE TABLE if not exists clientes(
 	
-    cliente_id LONG NOT NULL AUTO_INCREMENT,
+    cliente_id integer NOT NULL AUTO_INCREMENT primary key,
     username VARCHAR(50) NOT NULL UNIQUE,
     senha VARCHAR(75) NOT NULL,
 	nome VARCHAR(50) NOT NULL,
-    idade int NOT NULL,
     cpf varchar(15) NOT NULL,
     cidade VARCHAR(50) NOT NULL,
     endereco VARCHAR(100) NOT NULL,
     cep VARCHAR(20)
 	
-)
-ALTER TABLE clientes ADD UNIQUE(cliente_id)
-ALTER TABLE clientes ADD PRIMARY KEY(cliente_id)
+);
+
+drop table clientes
+ALTER TABLE clientes ADD UNIQUE(cliente_id);
+ALTER TABLE clientes ADD PRIMARY KEY(cliente_id);
+
 
 /*
 CREATE TABLE IF NOT EXISTS adocao(
@@ -40,26 +42,32 @@ CREATE TABLE IF NOT EXISTS adocao(
 )
 */
 
+
 CREATE TABLE IF NOT EXISTS chat(
 
-    chat_id LONG  NOT NULL AUTO_INCREMENT,
-    user1_id LONG NOT NULL,
-    user2_id LONG NOT NULL
+    chat_id INTEGER  NOT NULL AUTO_INCREMENT primary key,
+    user1_id integer NOT NULL,
+    user2_id integer NOT NULL
     
     
-)
+);
 
-ALTER TABLE chat ADD UNIQUE(chat_id)
-ALTER TABLE chat ADD PRIMARY KEY(chat_id)
+ALTER TABLE chat ADD UNIQUE(chat_id);
+ALTER TABLE chat ADD PRIMARY KEY(chat_id);
 
 CREATE TABLE IF NOT EXISTS mensagens(
 
-    id_mensagem LONG NOT NULL,
-    id_chat LONG NOT NULL,
-    id_remetente LONG NOT NULL,
+    id_mensagem int NOT NULL auto_increment,
+    id_chat INTEGER NOT NULL,
+    id_remetente integer NOT NULL,
     mensagem TEXT NOT NULL,
+    key message_length (mensagem(255)),
     FOREIGN KEY(id_chat) REFERENCES chat(chat_id)
-)
+);
 
-ALTER TABLE mensagens ADD UNIQUE(id_mensagem)
-ALTER TABLE mensagens ADD PRIMARY KEY(id_mensagem)
+ALTER TABLE mensagens ADD UNIQUE(id_mensagem);
+ALTER TABLE mensagens ADD PRIMARY KEY(id_mensagem);
+
+
+SELECT * FROM pets,
+SELECT * FROM clientes
